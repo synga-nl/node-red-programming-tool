b6771954e262ce4ea133781e174fca7433fa79ff
Roy Pouls <github@synga.nl>
2023-12-13 20:50:09 +0100

    - Add feature to also handle HH:MM:SS strings

--- src/Tools/Helpers/Time.ts
+++ src/Tools/Helpers/Time.ts
@@ -2,16 +2,18 @@ export class Time {
   /**
    * Checks if the current time is the time string + rangeInMinutes
    *
-   * @param stringTime e.g. "06:30"
+   * @param stringTime e.g. "06:30" or "06:30:56"
    * @param rangeInMinutes e.g. 10
    */
   isTimeInRange(stringTime: string, rangeInMinutes: number) {
     const currentTime: Date = new Date();
 
-    const [hours, minutes] = stringTime.split(':').map(Number);
+    const [hours, minutes, seconds] = stringTime.split(':').slice(0,3).map(Number);
     const givenTime: Date = new Date();
+
     givenTime.setHours(hours);
     givenTime.setMinutes(minutes);
+    givenTime.setSeconds(isNaN(seconds) ? 0 : seconds);
 
     const upperTimeBoundary: Date = new Date(currentTime.getTime() - rangeInMinutes * 60000 + 1); // Upper boundary = given time + range + 1 minute
 
--- src/__tests__/Helpers/TimeHelper.test.ts
+++ src/__tests__/Helpers/TimeHelper.test.ts
@@ -34,4 +34,15 @@ describe('Time Helper test', () => {
     date.setHours(date.getHours() + 1);
     expect(timeHelper.isTimeInRange(`${date.getHours()}:${date.getMinutes()}`, 10)).toBeFalsy();
   });
+
+  it('can handle HH:MM:SS strings', () => {
+    const timeHelper = new Time();
+
+    let date = new Date();
+    expect(timeHelper.isTimeInRange(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds}`, 10)).toBeTruthy();
+
+    date = new Date();
+    date.setMinutes(date.getMinutes() + 1);
+    expect(timeHelper.isTimeInRange(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds}`, 10)).toBeFalsy();
+  });
 });
